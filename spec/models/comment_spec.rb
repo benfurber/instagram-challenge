1af0b71f55c27bdd9c57ac178e21c8328c072db8
benfurber <ben@furber-consultancy.com>
2018-06-16 15:24:20 +0100

    Finished removing Comments and first new failing Comments test

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,9 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   describe 'Fields' do
-    it { is_expected.to have_db_column(:commentable_id).of_type(:integer) }
-    it { is_expected.to have_db_column(:commentable_type).of_type(:string) }
+    it 'commentable_id exists' do
+      expect(db_column(:commentable_id)).to be_a_kind_of(:integer)
+    end
+    it 'commentable_type exists' do
+      expect(db_column(:commentable_type)).to be_a_kind_of(:string)
+    end
 
-    it { is_expected.to belong_to(:commentable) }
+    it 'belongs to commentable' do
+      expect(Subject).to belong_to(:commentable)
+    end
   end
 end
